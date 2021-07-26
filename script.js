dc302a180ef96a5ab9a223c6b83c229f0770d8f8
Leandro Nogueira <leandro2903.lan@gmail.com>
2021-07-26 00:11:09 -0300

    formulário para cadastro de clientes

--- script.js
+++ script.js
@@ -54,3 +54,33 @@ function apagarFinalizados() {
   console.log(listaAdicionadas);
 }
 buttonApagaFinalizados.addEventListener('click', apagarFinalizados);
+
+
+
+// códigos para o formulário
+
+function createStateOptions () {
+  const stateSigla = ['AC', 'AL', 'AP', 'AM', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MT', 'MS', 'MG', 'PA', 'PB', 'PR', 'PE', 'PI', 'RJ', 'RN', 'RS', 'RO', 'RR', 'SC', 'SP', 'SE', 'TO'];
+  const stateLista = ['Acre', 'Alagoas', 'Amapá', 'Amazonas', 'Bahia', 'Ceará', 'Distrito Federal', 'Espírito Santo', 'Goiás', 'Maranhão', 'Mato Grosso', 'Mato Grosso do Sul', 'Minas Gerais', 'Pará', 'Paraíba', 'Paraná', 'Pernambuco', 'Piauí', 'Rio de Janeiro', 'Rio Grande do Norte', 'Rio Grande do Sul', 'Rondônia', 'Roraima', 'Santa Catarina', 'São Paulo', 'Sergipe', 'Tocantins'];
+  
+  const estado = document.querySelector('#estado');
+  
+    for (let i = 0; i < stateLista.length; i += 1 ) {
+      let stateName = document.createElement('option');
+      stateName.value = stateSigla[i];
+      stateName.innerText = stateLista[i];
+      document.querySelector('#estado').appendChild(stateName);
+    }
+}
+createStateOptions ()
+
+const button = document.querySelector('#enviar')
+const divPai = document.querySelector('main');
+const divConteudo = document.createElement('div')
+
+function printValues(event) {
+  event.preventDefault();
+  divConteudo.
+  divPai.appendChild(divConteudo);
+}
+button.addEventListener('click', printValues);
